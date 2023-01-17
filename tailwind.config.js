ff5e3da2d04b3cb6480659e2723aa2130eaff85b
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-17 19:41:33 +0900

    chore: Tailwind 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      main: '#91D3C8',
+      red: '#DF6B5A',
+      blue: '#364BB3',
+      grey: '#EAEAEA',
+      tab: '#FAFAFA',
+      sell: '#F3F8FE',
+      buy: '#FDF3F4',
+      black: '#000000',
+      'dark-grey': '#606060',
+    },
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#91D3C8',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
