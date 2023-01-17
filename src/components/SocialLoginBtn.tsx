e73c45bfa8a9d7ceff4ce30ad66237212aece873
권유리 <99jiyusu@gmail.com>
2023-01-18 01:44:14 +0900

    style: 소셜 로그인 컴포넌트 UI 구현

---
+++ src/components/SocialLoginBtn.tsx
@@ -0,0 +1,17 @@
+import Icon from '@/components/common/Icons';
+
+interface PropsType {
+  readonly socialType: string;
+}
+
+const SocialLoginBtn = ({ socialType }: PropsType) => {
+  return (
+    <button className="btn gap-8 bg-transparent hover:bg-main hover:text-white text-black font-bold btn-wide border-black/10 hover:border-transparent normal-case">
+      {socialType === 'google' && <Icon.Google />}
+      {/* 첫번째 글자를 대문자로 변환 */}
+      {socialType.charAt(0).toUpperCase() + socialType.slice(1)}로 계속하기
+    </button>
+  );
+};
+
+export default SocialLoginBtn;
