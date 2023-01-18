b76f3bb6e64c0b8e7a1b0598b3b9deffb749aace
권유리 <99jiyusu@gmail.com>
2023-01-18 22:15:13 +0900

    style: 로그인 페이지 디자인 일부 수정 - 페이지 padding 변경 - 로그인 버튼 width 변경

--- src/components/SocialLoginBtn.tsx
+++ src/components/SocialLoginBtn.tsx
@@ -6,7 +6,7 @@ interface PropsType {
 
 const SocialLoginBtn = ({ socialType }: PropsType) => {
   return (
-    <button className="btn gap-8 bg-transparent hover:bg-main hover:text-white text-black font-bold btn-wide border-black/10 hover:border-transparent normal-case">
+    <button className="btn w-96 gap-8 bg-transparent hover:bg-main hover:text-white text-black font-bold border-black/10 hover:border-transparent normal-case">
       {socialType === 'google' && <Icon.Google />}
       {/* 첫번째 글자를 대문자로 변환 */}
       {socialType.charAt(0).toUpperCase() + socialType.slice(1)}로 계속하기
