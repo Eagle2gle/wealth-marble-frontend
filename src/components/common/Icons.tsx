0ae02a29bade17dce6bb491f15d7b13e0da3d51e
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-19 20:07:47 +0900

    style: Left, Right 아이콘 추가

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -105,6 +105,32 @@ const Google = () => (
   </svg>
 );
 
-const Icon = { Home, Gavel, Market, User, Bell, Google };
+const Left = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+  </svg>
+);
+
+const Right = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+  </svg>
+);
+
+const Icon = { Home, Gavel, Market, User, Bell, Google, Left, Right };
 
 export default Icon;
