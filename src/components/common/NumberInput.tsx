aeba6e052d010750e2505d20194c780cf579c6e4
권유리 <99jiyusu@gmail.com>
2023-01-25 00:11:12 +0900

    feat: 숫자 입력 컴포넌트 +, - 동작 구현

--- src/components/common/NumberInput.tsx
+++ src/components/common/NumberInput.tsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
+
 interface PropsType {
-  value: number;
   size: 'small' | 'large';
+  value?: number;
 }
 
-const NumberInput = ({ value, size }: PropsType) => {
+// TODO: value -> 기본값 지정 시 필요할 수 있어서 놔둠
+const NumberInput = ({ size, value }: PropsType) => {
+  const [num, setNum] = useState<number>(0);
+
   return (
     <div className={`${size === 'small' ? 'w-28' : 'w-60'} flex flex-wrap h-10`}>
       <div className={`${size === 'small' ? 'w-4/6' : 'w-5/6'} flex h-full`}>
         <input
           type="text"
-          value={value}
+          value={num || ''}
           className="outline-main bg-white text-sm text-gray-900 text-center border border-black/20 focus:border-gray-600 border-r-0 rounded-l-md w-full"
           onChange={(e) => {
-            // console.log(e);
+            setNum(Number(e.target.value));
           }}
         />
       </div>
@@ -20,12 +25,14 @@ const NumberInput = ({ value, size }: PropsType) => {
         <button
           type="button"
           className="h-1/2 text-black/20 text-center text-md font-semibold rounded-tr-md px-3 focus:outline-none border border-b-0 border-black/20 hover:border-main hover:bg-main leading-none"
+          onClick={() => setNum(num + 1)}
         >
           +
         </button>
         <button
           type="button"
           className="h-1/2 text-black/20 text-center text-md font-semibold rounded-br-md px-3 focus:outline-none border border-black/20 hover:border-main hover:bg-main leading-none"
+          onClick={() => setNum(num - 1)}
         >
           -
         </button>
