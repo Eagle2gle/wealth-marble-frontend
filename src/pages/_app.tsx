a9b7dddc0e3415841ecea3b25ff235c25b43e499
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-25 01:10:26 +0900

    refactor: redux wrapper 방식 변경
    - deprecated된 `wrapper.withRedux()` 대신 `wrapper.useWrappedStore()` 사용
    - https://stackoverflow.com/a/73861569

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css';
 
+import { Provider } from 'react-redux';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -9,13 +11,17 @@ import type { AppProps } from 'next/app';
 
 const queryClient = new QueryClient();
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
-export default wrapper.withRedux(App);
+export default App;
