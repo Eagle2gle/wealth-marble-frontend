853471127c8b782412b50c91074c44fff8b7749a
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-30 02:39:03 +0900

    feat: 공모 상세 정보 응답 타입 분리

---
+++ src/types/response.d.ts
@@ -0,0 +1,18 @@
+export type CahootDetailInfoType = {
+  id: number;
+  title: string;
+  themeLocation: string;
+  themeBuilding: string;
+  location: string;
+  expectedMonth: number;
+  expectedTotalCost: number;
+  shortDescription: string;
+  descritption: string;
+  status: string;
+  stockStart: string;
+  stockEnd: string;
+  stockPrice: number;
+  stockNum: number;
+  competitionRate: number;
+  images: string[];
+};
