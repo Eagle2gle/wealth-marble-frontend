d6a00d34c409bf8bb0d090864ebf90a3dbabfa02
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-29 20:51:47 +0900

    style: Carousel 패딩 제거
    - 불필요한 패딩 제거

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -21,13 +21,13 @@ const Carousel = ({ children, itemCount }: CarouselProps) => {
 
   return (
     <div className="flex items-center">
-      <button onClick={onLeftClick} className="btn btn-circle btn-ghost hidden md:flex">
+      <button onClick={onLeftClick} className="btn-ghost btn-circle btn hidden md:flex">
         <Icon.Left />
       </button>
-      <div className="carousel w-full space-x-2 px-4" ref={carouselRef}>
+      <div className="carousel w-full space-x-2" ref={carouselRef}>
         {children}
       </div>
-      <button onClick={onRightClick} className="btn btn-circle btn-ghost hidden md:flex">
+      <button onClick={onRightClick} className="btn-ghost btn-circle btn hidden md:flex">
         <Icon.Right />
       </button>
     </div>
