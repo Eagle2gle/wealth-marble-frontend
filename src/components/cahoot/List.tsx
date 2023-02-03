28c3d1158f9cbeff3238761c9a9ab1cd95bcc6b6
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-03 19:27:31 +0900

    feat: 공모 목록 데이터 연동

--- src/components/cahoot/List.tsx
+++ src/components/cahoot/List.tsx
@@ -1,26 +1,27 @@
+import { useState } from 'react';
+
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { useSuspendedQuery } from '@/hooks/useSuspendedQuery';
+import type { CahootListType } from '@/types/cahoot';
+import type { Response } from '@/types/response';
 import classNames from '@/utils/classnames';
 import dateFormat from '@/utils/dateFormat';
 
 import Icon from '../common/Icons';
 import Search from '../common/Search';
 
-type MockDataType = {
-  id: number;
-  title: string;
-  deadline: string;
-  price: number;
-  amount: number;
-  competitiveRate: number;
-  location: string;
-  bookmarked: boolean;
-}[];
-
 const CahootList = () => {
-  const { data } = useSuspendedQuery<MockDataType>(['cahootListData'], () =>
-    fetch(`${process.env.NEXT_PUBLIC_HOST}/cahootListData.json`).then((res) => res.json())
+  const [keyword, setKeyword] = useState('');
+  const {
+    data: {
+      data: { result },
+    },
+  } = useSuspendedQuery<Response<CahootListType>>(['cahoot/list', keyword], () =>
+    fetch(
+      `${process.env.NEXT_PUBLIC_HOST}/api/cahoots?status=ongoing&offset=0&keyword=${keyword}`
+    ).then((res) => res.json())
   );
 
   const onBookmarkClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
@@ -29,6 +30,10 @@ const CahootList = () => {
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     console.log('submitted');
+    const target = e.target as typeof e.target & {
+      search: { value: string };
+    };
+    setKeyword(target.search.value);
   };
 
   return (
@@ -39,60 +44,70 @@ const CahootList = () => {
           <Search />
         </form>
       </div>
-      {data.map(({ id, amount, bookmarked, competitiveRate, deadline, location, price, title }) => (
-        <Link
-          href={`/cahoots/detail/${id}`}
-          key={id}
-          className="flex gap-6 rounded-lg border border-grey shadow-md md:p-2"
-        >
-          <div className="avatar">
-            <div className="w-32 rounded-l-lg bg-dark-grey md:rounded-lg"></div>
-          </div>
-          <div className="relative flex w-full flex-col justify-center gap-1 overflow-hidden py-2 pr-4 text-sm md:text-base">
-            <div className="flex flex-col border-b border-grey pb-2.5 font-bold md:flex-row md:gap-2">
-              <span className="break-keep text-main md:text-black">{location}</span>
-              <div className="mx-2 hidden border-l-2 py-0.5 md:block"></div>
-              <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">{title}</span>
-              <div className="absolute right-3 top-1 md:top-auto">
-                <button
-                  onClick={onBookmarkClick}
-                  className={classNames(
-                    'btn-ghost btn-xs btn-circle btn',
-                    bookmarked ? 'fill-main text-main' : 'fill-none'
-                  )}
-                >
-                  <Icon.Bookmark />
-                </button>
+      {result.map(
+        ({ id, stockNum, competitionRate, stockEnd, location, stockPrice, title, images }) => (
+          <Link
+            href={`/cahoots/detail/${id}`}
+            key={id}
+            className="flex gap-6 rounded-lg border border-grey shadow-md md:p-2"
+          >
+            <div className="avatar">
+              <div className="w-32 rounded-l-lg md:rounded-lg"></div>
+              <Image
+                src={images[0]}
+                alt=""
+                className="rounded-l-lg md:rounded-lg"
+                fill
+                sizes="128px"
+              />
+            </div>
+            <div className="relative flex w-full flex-col justify-center gap-1 overflow-hidden py-2 pr-4 text-sm md:text-base">
+              <div className="flex flex-col border-b border-grey pb-2.5 font-bold md:flex-row md:gap-2">
+                <span className="break-keep text-main md:text-black">{location}</span>
+                <div className="mx-2 hidden border-l-2 py-0.5 md:block"></div>
+                <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">{title}</span>
+                <div className="absolute right-3 top-1 md:top-auto">
+                  <button
+                    onClick={onBookmarkClick}
+                    className={classNames(
+                      'btn-ghost btn-xs btn-circle btn',
+                      // bookmarked ? 'fill-main text-main' : 'fill-none'
+                      'fill-none'
+                    )}
+                  >
+                    <Icon.Bookmark />
+                  </button>
+                </div>
+              </div>
+              <div className="flex flex-col gap-1 md:flex-row md:gap-4">
+                <div className="flex flex-1 justify-between border-b border-grey px-1 pb-1.5">
+                  공모가
+                  <span>{stockPrice.toLocaleString()}원</span>
+                </div>
+                <div className="flex flex-1 justify-between border-b border-grey px-1 pb-1.5">
+                  공모 수량
+                  <span>{stockNum.toLocaleString()}주</span>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <div className="flex flex-1 justify-center px-1 md:justify-between">
+                  <span className="hidden md:block">경쟁률</span>
+                  <span className="font-bold text-main">
+                    {competitionRate}%<span className="md:hidden"> 달성</span>
+                  </span>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-1 md:justify-between">
+                  <span className="hidden md:block">공모 마감일</span>
+                  <span>
+                    {dateFormat(stockEnd)}
+                    <span className="md:hidden"> 마감</span>
+                  </span>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col gap-1 md:flex-row md:gap-4">
-              <div className="flex flex-1 justify-between border-b border-grey px-1 pb-1.5">
-                공모가
-                <span>{price.toLocaleString()}원</span>
-              </div>
-              <div className="flex flex-1 justify-between border-b border-grey px-1 pb-1.5">
-                공모 수량
-                <span>{amount.toLocaleString()}주</span>
-              </div>
-            </div>
-            <div className="flex gap-4">
-              <div className="flex flex-1 justify-center px-1 md:justify-between">
-                <span className="hidden md:block">경쟁률</span>
-                <span className="font-bold text-main">
-                  {competitiveRate}%<span className="md:hidden"> 달성</span>
-                </span>
-              </div>
-              <div className="flex flex-1 items-center justify-center px-1 md:justify-between">
-                <span className="hidden md:block">공모 마감일</span>
-                <span>
-                  {dateFormat(deadline)}
-                  <span className="md:hidden"> 마감</span>
-                </span>
-              </div>
-            </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        )
+      )}
     </div>
   );
 };
--- src/components/cahoot/Recap.tsx
+++ src/components/cahoot/Recap.tsx
@@ -1,29 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { useSuspendedQuery } from '@/hooks/useSuspendedQuery';
+import type { CahootListType } from '@/types/cahoot';
+import type { Response } from '@/types/response';
 import dateFormat from '@/utils/dateFormat';
 
 import Carousel from '../common/Carousel';
 
-type MockDataType = {
-  id: number;
-  title: string;
-  deadline: string;
-  price: number;
-  amount: number;
-  competitiveRate: number;
-}[];
-
 const Recap = () => {
-  const { data } = useSuspendedQuery<MockDataType>(['cahootRecapData'], () =>
-    fetch(`${process.env.NEXT_PUBLIC_HOST}/cahootRecapData.json`).then((res) => res.json())
+  const {
+    data: {
+      data: { result },
+    },
+  } = useSuspendedQuery<Response<CahootListType>>(['cahoot/recap'], () =>
+    fetch(`${process.env.NEXT_PUBLIC_HOST}/api/cahoots?status=ended`).then((res) => res.json())
   );
 
   return (
     <div className="flex flex-col gap-2 bg-main-light p-4 md:p-0 md:py-4">
       <label className="font-bold md:px-4">공모 다시보기 어때요</label>
-      <Carousel itemCount={data?.length ?? 0}>
-        {data.map(({ id, title, deadline, amount, competitiveRate, price }) => (
+      <Carousel itemCount={result.length}>
+        {result.map(({ id, title, stockEnd, stockNum, competitionRate, stockPrice, images }) => (
           <Link
             key={id}
             href={`/cahoots/detail/${id}`}
@@ -32,25 +30,26 @@ const Recap = () => {
             <div className="avatar z-0">
               {/* 이미지 */}
               <div className="w-32 rounded-t bg-dark-grey"></div>
+              <Image src={images[0]} className="rounded-t" alt="" fill sizes="128px" />
               <span className="absolute bottom-5 w-32 overflow-hidden overflow-ellipsis whitespace-nowrap px-2 text-xs text-white">
                 {title}
               </span>
               <span className="absolute bottom-0 w-full bg-black/25 text-center text-xs text-white">
-                {dateFormat(deadline)}
+                {dateFormat(stockEnd)}
               </span>
             </div>
             <div className="flex w-full flex-col gap-1 rounded-b bg-white p-2 text-xs font-normal">
               <div className="flex justify-between">
                 공모가
-                <span>{price.toLocaleString()}</span>
+                <span>{stockPrice.toLocaleString()}</span>
               </div>
               <div className="flex justify-between">
                 공모 수량
-                <span>{amount.toLocaleString()} 주</span>
+                <span>{stockNum.toLocaleString()} 주</span>
               </div>
               <div className="flex justify-between">
                 경쟁률
-                <span className="font-bold text-main">{competitiveRate}%</span>
+                <span className="font-bold text-main">{competitionRate}%</span>
               </div>
             </div>
           </Link>
