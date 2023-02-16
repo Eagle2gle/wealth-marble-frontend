a216eb4915135278cb873ebec0e031fb6231715e
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-16 21:01:13 +0900

    style: 관심가는 휴양지 추가 버튼

--- src/components/market/Interests.tsx
+++ src/components/market/Interests.tsx
@@ -17,17 +17,21 @@ const Interests = () => {
     e.preventDefault();
   };
 
-  return interests.length ? (
+  return (
     <div className="flex flex-col gap-2 px-3 md:px-0">
       <label className="font-bold">관심가는 휴양지</label>
       <Carousel itemCount={interests.length}>
+        <button className="carousel-item h-24 w-52 items-center justify-center gap-1 rounded bg-grey text-sm font-semibold">
+          <Icon.PlusCircle />
+          휴양지를 추가해 주세요
+        </button>
         {interests.map(({ id, title }) => (
           <Link
             key={id}
             href={`/markets/detail/${id}`}
             className="carousel-item flex-col items-center font-semibold"
           >
-            <div className="from-blue to-sell relative w-52 break-keep rounded bg-gradient-to-br from-blue-start to-blue-end p-6 text-center text-sm text-white">
+            <div className="relative flex h-24 w-52 items-center break-keep rounded bg-gradient-to-br from-blue-start to-blue-end p-6 text-center text-sm text-white">
               {title}
               <div className="absolute right-1 top-1">
                 <button
@@ -42,7 +46,7 @@ const Interests = () => {
         ))}
       </Carousel>
     </div>
-  ) : null;
+  );
 };
 
 export default Interests;
