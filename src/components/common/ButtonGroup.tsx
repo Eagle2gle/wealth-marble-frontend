7c63421b467de658f5a47c64a25e42960c4bf1b6
권유리 <99jiyusu@gmail.com>
2023-02-16 09:28:34 +0900

    feat: ButtonGroup 컴포넌트 구현

---
+++ src/components/common/ButtonGroup.tsx
@@ -0,0 +1,34 @@
+import classNames from '@/utils/classnames';
+
+interface PropsType {
+  items: readonly string[];
+  currentItem: string;
+  changeItem: (item: string) => void;
+}
+
+const ButtonGroup = ({ items, currentItem, changeItem }: PropsType) => {
+  const onClickButton = (item: string) => {
+    changeItem(item);
+  };
+
+  return (
+    <div className="flex h-6 gap-2">
+      {items.map((item, index) => (
+        <button
+          key={index}
+          className={classNames(
+            'w-12 truncate rounded-lg px-1 text-[10px]',
+            currentItem === item
+              ? 'bg-main text-white'
+              : 'btn-secondary border border-grey bg-white font-medium text-black/60'
+          )}
+          onClick={() => onClickButton(item)}
+        >
+          {item}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ButtonGroup;
