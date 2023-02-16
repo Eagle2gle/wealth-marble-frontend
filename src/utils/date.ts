af232d1fb97aeaf423bfce681d55ed2ac1c3b30d
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-17 01:49:17 +0900

    feat: formatTime 함수 구현

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,3 +3,10 @@ export const formatDate = (date: string) =>
     .format(new Date(date || 0))
     .replace(/\s|\.$/g, '');
 
+export const formatTime = (date: string) =>
+  Intl.DateTimeFormat('ko-KR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  }).format(new Date(date || 0));
