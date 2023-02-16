df5af9142a07c0a4f87dbab3f349a8a9c2e0a8bd
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-17 01:48:54 +0900

    refactor: dateFormat 함수명 변경
    - formatDate로 변경
    - dateFormat -> date 파일명 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,5 @@
-const dateFormat = (date: string) =>
+export const formatDate = (date: string) =>
   Intl.DateTimeFormat('ko-KR', { dateStyle: 'short' })
     .format(new Date(date || 0))
     .replace(/\s|\.$/g, '');
 
-export default dateFormat;
