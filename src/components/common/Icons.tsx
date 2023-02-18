7bf318a5bd48ed04f64b8cd434d8bf210e85e7cb
권유리 <99jiyusu@gmail.com>
2023-02-18 11:58:17 +0900

    chore: 토글 아이콘 크기 통일

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -138,7 +138,7 @@ const Up = () => (
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="h-4 w-4"
+    className="h-3 w-3"
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
   </svg>
