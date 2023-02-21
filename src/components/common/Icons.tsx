c90fd134fe1380fb431114d8088e110a764acd07
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-21 20:48:22 +0900

    style: 시계 아이콘 추가

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -270,6 +270,23 @@ const PlusCircle = () => (
   </svg>
 );
 
+const Clock = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-4 w-4"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
 const Icon = {
   Home,
   Gavel,
@@ -288,6 +305,7 @@ const Icon = {
   Minus,
   Plus,
   PlusCircle,
+  Clock,
 };
 
 export default Icon;
