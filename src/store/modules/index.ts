0055b86552d6558b59fc09e7decb72172fc4a046
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-01 00:54:45 +0900

    feat: userSlice redux-persist 적용

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,11 +1,21 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import cahootOrderSlice from './cahootOrder';
+import userSlice from './user';
 
 const combinedReducer = combineReducers({
   [cahootOrderSlice.name]: cahootOrderSlice.reducer,
+  [userSlice.name]: userSlice.reducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
 
 const rootReducer: typeof combinedReducer = (state, action) => {
   if (action.type === HYDRATE) {
@@ -18,4 +28,5 @@ const rootReducer: typeof combinedReducer = (state, action) => {
     return combinedReducer(state, action);
   }
 };
-export default rootReducer;
+
+export default persistReducer(persistConfig, rootReducer);
