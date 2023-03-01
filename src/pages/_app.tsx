6322a59e263bdc12587bb40c8fca0b83b4131cc1
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-02 03:21:18 +0900

    feat: React Query prefetching 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,30 @@
 import '@/styles/globals.css';
 
+import { useState } from 'react';
 import { Provider } from 'react-redux';
 
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-import type { AppProps } from 'next/app';
 
 import wrapper from '../store';
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { suspense: true, useErrorBoundary: true } },
-});
+import type { AppProps } from 'next/app';
 
 const App = ({ Component, ...rest }: AppProps) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { suspense: true, useErrorBoundary: true } },
+      })
+  );
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
+        </Hydrate>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
