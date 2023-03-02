2d91a3dc22b1e89fd345d9f6ba383edb01595b3d
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-02 20:56:13 +0900

    feat: 페이지 접속 시 토큰 유효성 검사
    - 유효하지 않은 경우 로그아웃 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '@/styles/globals.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 
+import { api } from '@/libs/client/api';
+import { logout } from '@/store/modules/user';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import wrapper from '../store';
-
 import type { AppProps } from 'next/app';
+
+import wrapper from '../store';
 
 const App = ({ Component, ...rest }: AppProps) => {
   const [queryClient] = useState(
@@ -19,6 +21,14 @@ const App = ({ Component, ...rest }: AppProps) => {
   );
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
+  useEffect(() => {
+    const token = store.getState().user.token;
+    if (token) {
+      api
+        .get('auth/users/me', { headers: { Authorization: `Bearer ${token}` } })
+        .catch(() => store.dispatch(logout()));
+    }
+  }, [store]);
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
