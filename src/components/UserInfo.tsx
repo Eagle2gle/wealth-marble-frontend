008789d61059a3ce0065ac7285698d19f3812d75
권유리 <99jiyusu@gmail.com>
2023-03-02 16:50:51 +0900

    style: border 굵기 변경

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,11 +1,12 @@
 import Icon from '@/components/common/Icons';
+
 const UserInfo = () => {
   return (
     <>
       {/* only desktop */}
       <div className="my-2 hidden rounded-md border border-grey px-6 py-6 pt-4 md:block">
         <p className="text-lg font-bold text-main">홍길동님</p>
-        <hr className="my-2 border border-grey"></hr>
+        <hr className="border-1 my-2 border-grey"></hr>
         <div className="flex flex-col gap-3">
           <p className="flex gap-6 text-sm">
             <span className="w-20">보유 캐쉬:</span>
@@ -37,7 +38,7 @@ const UserInfo = () => {
         </div>
         <div className="my-6 mx-12 rounded-md border border-main px-6 pt-4 pb-6">
           <p className="text-xl font-bold">내 지갑</p>
-          <hr className="my-2 border border-grey"></hr>
+          <hr className="border-1 my-2 border-grey"></hr>
           <p className="py-1 text-xs font-black">보유캐쉬</p>
           <p className="flex items-center gap-1">
             <span>
@@ -47,7 +48,7 @@ const UserInfo = () => {
               {Number(4000000).toLocaleString()}원
             </span>
           </p>
-          <hr className="my-2 border border-grey"></hr>
+          <hr className="border-1 my-2 border-grey"></hr>
           <p className="py-1 text-xs font-black">총 지분 가치</p>
           <p className="flex items-center gap-1">
             <span>
