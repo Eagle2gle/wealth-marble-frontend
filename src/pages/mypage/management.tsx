7c351d0fbeb06e56db50936e96014f30098ffbde
권유리 <99jiyusu@gmail.com>
2023-03-04 03:36:11 +0900

    feat: 개인정보 관리 페이지 데이터 연결

--- src/pages/mypage/management.tsx
+++ src/pages/mypage/management.tsx
@@ -1,8 +1,27 @@
 import Image from 'next/image';
 
 import Icon from '@/components/common/Icons';
+import { useSuspendedQuery } from '@/hooks/useSuspendedQuery';
+import { api } from '@/libs/client/api';
+import { useTypeSelector } from '@/store';
+import { Response } from '@/types/response';
+import { UserInfoType } from '@/types/user';
 
 export default function Management() {
+  const token = useTypeSelector((state) => state.user.token);
+  const { data } = useSuspendedQuery<Response<UserInfoType>>(
+    [`user/info`],
+    () =>
+      api
+        .get(`auth/users/me`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .json<Response<UserInfoType>>(),
+    { enabled: !!token }
+  );
+
+  console.log(data);
+
   return (
     <div>
       <section className="flex items-center gap-2.5 px-10 py-4 text-center">
@@ -14,7 +33,7 @@ export default function Management() {
       </section>
       <hr className="border-1 mb-2 border-grey" />
       <main className="flex items-center justify-center p-4">
-        <div className="flex w-80 flex-col">
+        <div className="flex w-96 flex-col">
           <h2 className="items-start text-base font-bold">기본 정보 관리</h2>
           <div className="avatar my-4 w-32 self-center">
             <div className="w-32"></div>
@@ -34,7 +53,7 @@ export default function Management() {
               <span className="label-text text-xs">이름</span>
             </label>
             <p className="rounded-lg border border-black/10 bg-black/5 py-2 px-4 text-dark-grey">
-              권유리
+              {data?.data.username}
             </p>
           </div>
           {/* 가입 이메일 */}
@@ -43,11 +62,17 @@ export default function Management() {
               <span className="label-text text-xs">가입 이메일</span>
             </label>
             <p className="flex gap-2">
-              <span className="w-2/3 rounded-lg border border-black/10 bg-black/5 py-2 px-4 text-dark-grey">
-                test@gmail.com
+              <span
+                title={data?.data.email}
+                className="w-2/3 truncate rounded-lg border border-black/10 bg-black/5 py-2 px-4 text-dark-grey"
+              >
+                {data?.data.email}
               </span>
-              <span className="w-1/3 rounded-lg bg-black/30 py-2 px-4 text-center text-white">
-                구글 연동
+              <span
+                title={data?.data.providerType}
+                className="w-1/3 truncate rounded-lg bg-black/30 py-3 px-4 text-center text-sm text-white"
+              >
+                {data?.data.providerType} 연동
               </span>
             </p>
           </div>
@@ -58,10 +83,10 @@ export default function Management() {
               <span className="label-text text-xs">권한</span>
             </label>
             <p className="rounded-lg border border-grey bg-black/5 py-2 px-4 text-dark-grey">
-              일반 사용자
+              {data?.data.role}
             </p>
           </div>
-          <button className="btn-primary btn-block btn-sm btn my-6 w-80">업데이트 하기</button>
+          <button className="btn-primary btn-block btn-sm btn my-6 ">업데이트 하기</button>
         </div>
       </main>
     </div>
