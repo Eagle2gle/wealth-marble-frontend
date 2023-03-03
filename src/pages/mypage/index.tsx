b1e8cb41af90596a919086e2b1f751152c94ad4a
권유리 <99jiyusu@gmail.com>
2023-03-03 23:57:21 +0900

    feat: 로그인 여부에 따라 마이페이지 화면 분기 처리

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -9,88 +9,98 @@ import ParticapatedContest from '@/components/mypage/ParticipatedContest';
 import Stocks from '@/components/mypage/Stocks';
 import Transactions from '@/components/mypage/Transactions';
 import UserInfo from '@/components/mypage/UserInfo';
+import { useTypeSelector } from '@/store';
 import { ErrorBoundary } from '@sentry/nextjs';
 
 const Mypage = () => {
+  const id = useTypeSelector((state) => state.user.id);
+
   return (
-    <Layout>
-      <div className="mb-60 flex flex-col gap-6 md:mb-0">
-        <Suspense fallback={<p>로딩...</p>}>
-          <ErrorBoundary
-            fallback={({ resetError, error }) => (
-              <>
-                <p>{error.message}</p>
-                <button onClick={() => resetError()}>reset</button>
-              </>
-            )}
-          >
-            <div className="flex justify-between md:py-12">
-              <div className="hidden w-1/4 md:block">
-                <p className="text-xl font-black">마이페이지</p>
-                <div className="avatar my-4">
-                  <div className="w-32 rounded-l-lg md:rounded-lg"></div>
-                  {/* {images[0] && ( */}
-                  <Image
-                    src="/images/thumbnail.jpeg"
-                    alt=""
-                    className="rounded-full"
-                    fill
-                    sizes="128px"
-                  />
-                  {/* )} */}
+    <>
+      {id ? (
+        <Layout>
+          <div className="mb-60 flex flex-col gap-6 md:mb-0">
+            <Suspense fallback={<p>로딩...</p>}>
+              <ErrorBoundary
+                fallback={({ resetError, error }) => (
+                  <>
+                    <p>{error.message}</p>
+                    <button onClick={() => resetError()}>reset</button>
+                  </>
+                )}
+              >
+                <div className="flex justify-between md:py-12">
+                  <div className="hidden w-1/4 md:block">
+                    <p className="text-xl font-black">마이페이지</p>
+                    <div className="avatar my-4">
+                      <div className="w-32 rounded-l-lg md:rounded-lg"></div>
+                      {/* {images[0] && ( */}
+                      <Image
+                        src="/images/thumbnail.jpeg"
+                        alt=""
+                        className="rounded-full"
+                        fill
+                        sizes="128px"
+                      />
+                      {/* )} */}
+                    </div>
+                    <ul className="menu w-4/5 bg-base-100">
+                      <hr className="border-1 my-2 border-main"></hr>
+                      <li>
+                        <Link href="/mypage/management" className="font-medium">
+                          내 정보
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/mypage/cahoots" className="font-medium">
+                          공모 내역
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/mypage/transactions" className="font-medium">
+                          거래 현황
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/mypage/stocks" className="font-medium">
+                          자산 현황
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/cahoots/create" className="font-medium">
+                          공모 등록
+                        </Link>
+                      </li>
+                      <hr className="border-1 my-2 border-main"></hr>
+                      <li>
+                        <Link href="/" className="font-medium">
+                          로그아웃
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="flex w-full flex-col md:w-3/4">
+                    <UserInfo />
+                    <ParticapatedContest />
+                    <Transactions />
+                    <Stocks />
+                    <Link
+                      href="/cahoots/create"
+                      className="mx-6 my-2 ml-auto block w-40 font-medium text-grey-middle md:hidden"
+                    >
+                      공모 등록하러가기 {'>'}
+                    </Link>
+                  </div>
                 </div>
-                <ul className="menu w-4/5 bg-base-100">
-                  <hr className="border-1 my-2 border-main"></hr>
-                  <li>
-                    <Link href="/mypage/management" className="font-medium">
-                      내 정보
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/mypage/cahoots" className="font-medium">
-                      공모 내역
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/mypage/transactions" className="font-medium">
-                      거래 현황
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/mypage/stocks" className="font-medium">
-                      자산 현황
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/cahoots/create" className="font-medium">
-                      공모 등록
-                    </Link>
-                  </li>
-                  <hr className="border-1 my-2 border-main"></hr>
-                  <li>
-                    <Link href="/" className="font-medium">
-                      로그아웃
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="flex w-full flex-col md:w-3/4">
-                <UserInfo />
-                <ParticapatedContest />
-                <Transactions />
-                <Stocks />
-                <Link
-                  href="/cahoots/create"
-                  className="mx-6 my-2 ml-auto block w-40 font-medium text-grey-middle md:hidden"
-                >
-                  공모 등록하러가기 {'>'}
-                </Link>
-              </div>
-            </div>
-          </ErrorBoundary>
-        </Suspense>
-      </div>
-    </Layout>
+              </ErrorBoundary>
+            </Suspense>
+          </div>
+        </Layout>
+      ) : (
+        // TODO: 로그인 X시 띄울 화면 기획 필요
+        <div>로그인 후 이용해주세요</div>
+      )}
+    </>
   );
 };
 
