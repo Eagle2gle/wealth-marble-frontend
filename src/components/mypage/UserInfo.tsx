8c6f1787b848e493765640d3ed450b4b92fa56da
권유리 <99jiyusu@gmail.com>
2023-03-04 01:02:12 +0900

    feat: 내 정보(UserInfo) 컴포넌트 API 연동

--- src/components/mypage/UserInfo.tsx
+++ src/components/mypage/UserInfo.tsx
@@ -1,33 +1,57 @@
 import Link from 'next/link';
 
 import Icon from '@/components/common/Icons';
+import { useSuspendedQuery } from '@/hooks/useSuspendedQuery';
+import { api } from '@/libs/client/api';
+import { Response } from '@/types/response';
+import { UserInfoType } from '@/types/user';
 
-const UserInfo = () => {
+interface PropsType {
+  token: string | undefined;
+}
+
+const UserInfo = ({ token }: PropsType) => {
+  const { data } = useSuspendedQuery<Response<UserInfoType>>(
+    [`user/info`],
+    () =>
+      api
+        .get(`auth/users/me`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .json<Response<UserInfoType>>(),
+    { enabled: !!token }
+  );
+
   return (
     <>
       {/* only desktop */}
       <div className="my-2 hidden rounded-md border border-grey px-6 py-6 pt-4 md:block">
-        <p className="text-lg font-bold text-main">홍길동님</p>
+        <p className="text-lg font-bold text-main">{data?.data.username}님</p>
         <hr className="border-1 my-2 border-grey"></hr>
         <div className="flex flex-col gap-3">
           <p className="flex gap-6 text-sm">
             <span className="w-20">보유 캐쉬:</span>
-            <span className="font-bold text-main">{Number(4000000).toLocaleString()}원</span>
+            <span className="font-bold text-main">
+              {Number(data?.data.cash).toLocaleString()}원
+            </span>
           </p>
           <p className="flex gap-6 text-sm">
             <span className="w-20">총 지분 가치:</span>
-            <span className="font-bold text-main">{Number(4000000).toLocaleString()}원</span>
+            <span className="font-bold text-main">
+              {Number(data?.data.value).toLocaleString()}원
+            </span>
           </p>
           <div className="flex gap-6 text-sm">
             <span className="w-20">Email:</span>
             <div className="flex flex-col gap-1">
-              <span className="">test@gmail.com</span>
-              <span className="">구글 연동</span>
+              <span className="">{data?.data.email}</span>
+              <span className="">{data?.data.providerType} 연동</span>
             </div>
           </div>
           <p className="flex gap-6 text-sm">
             <span className="w-20">권한:</span>
-            <span className="">사용자</span>
+            {/* TODO: 들어올 수 있는 값 확인 후 한글로 변환 */}
+            <span className="">{data?.data.role}</span>
             <button className="btn-primary btn-xs btn ml-auto">운영자 신청</button>
           </p>
         </div>
@@ -37,12 +61,12 @@ const UserInfo = () => {
         <Link href={`/mypage/management`}>
           <div className="border-y-1 border border-grey bg-black/5 py-2 px-4">
             <p className="flex text-base font-black">
-              <span>홍길동님</span>
+              <span>{data?.data.username}님</span>
               <span>
                 <Icon.Right />
               </span>
             </p>
-            <p className="text-xs text-grey-middle">test@gmail.com</p>
+            <p className="text-xs text-grey-middle">{data?.data.email}</p>
           </div>
         </Link>
         <div className="my-6 mx-12 rounded-md border border-main px-6 pt-4 pb-6">
@@ -54,7 +78,7 @@ const UserInfo = () => {
               <Icon.Money />
             </span>
             <span className="text-base font-bold text-main">
-              {Number(4000000).toLocaleString()}원
+              {Number(data?.data.cash).toLocaleString()}원
             </span>
           </p>
           <hr className="border-1 my-2 border-grey"></hr>
@@ -64,7 +88,7 @@ const UserInfo = () => {
               <Icon.Money />
             </span>
             <span className="text-base font-bold text-main">
-              {Number(4000000).toLocaleString()}원
+              {Number(data?.data.value).toLocaleString()}원
             </span>
           </p>
         </div>
