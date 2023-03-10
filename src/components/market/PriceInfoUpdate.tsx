ec19615bd5de0028149034602bcdbe46643543d8
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-10 22:06:20 +0900

    feat: 마켓 가격 정보 API 연동

--- src/components/market/PriceInfoUpdate.tsx
+++ src/components/market/PriceInfoUpdate.tsx
@@ -1,40 +1,12 @@
-import { useSuspendedQuery } from '@/hooks/useSuspendedQuery';
-import { Response } from '@/types/response';
 import { formatTimeWithoutSecond } from '@/utils/date';
 
 import Icon from '../common/Icons';
 
-type MockType = {
-  updatedAt: string;
-  result: {
-    id: number;
-    title: string;
-    price: number;
-    volume: number;
-    images: string[];
-    diff: number;
-  }[];
-};
-
-interface PriceInfoItemProps {
-  type: 'price' | 'percent';
-  order: 'asc' | 'desc';
-}
-
-const PriceInfoUpdate = ({ order, type }: PriceInfoItemProps) => {
-  const {
-    data: {
-      data: { updatedAt },
-    },
-  } = useSuspendedQuery<Response<MockType>>(['market/price', type, order], () =>
-    fetch(
-      `${process.env.NEXT_PUBLIC_HOST}/api/mock/markets/price?type=${type}&order=${order}`
-    ).then((res) => res.json())
-  );
+const PriceInfoUpdate = () => {
   return (
     <div className="flex gap-1 text-xs text-black/50 md:-mb-2">
       <Icon.Clock />
-      {formatTimeWithoutSecond(updatedAt)} 업데이트
+      {formatTimeWithoutSecond(new Date().toISOString())} 업데이트
     </div>
   );
 };
