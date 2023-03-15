04a6605832af6efdfecd376ee21e217cb919b4ed
권유리 <99jiyusu@gmail.com>
2023-03-16 00:11:32 +0900

    feat: 마이페이지, 공모생성페이지 뒤로가기 버튼 적용

--- src/pages/mypage/management.tsx
+++ src/pages/mypage/management.tsx
@@ -1,20 +1,13 @@
 import { Suspense } from 'react';
 
-import Icon from '@/components/common/Icons';
+import HeaderWithBackButton from '@/components/mypage/HeaderWithBackButton';
 import UserInfoManagement from '@/components/mypage/UserInfoManagement';
 import wrapper from '@/store';
 
 export default function Management() {
   return (
     <div>
-      <section className="flex items-center gap-2.5 px-10 py-4 text-center">
-        <div className="w-1/12">
-          {/* TODO: 뒤로가기 */}
-          <Icon.Left />
-        </div>
-        <h1 className="w-11/12 pr-12 text-xl font-bold">개인정보 관리</h1>
-      </section>
-      <hr className="border-1 mb-2 border-grey" />
+      <HeaderWithBackButton title="개인정보 관리" />
       <Suspense fallback={<p>로딩...</p>}>
         <UserInfoManagement />
       </Suspense>
