97417c0c573fa1421fb3861ce980f11f414949a5
권유리 <99jiyusu@gmail.com>
2023-03-17 10:20:41 +0900

    style: 휴양지 Top5 기준 토글 버튼 추가

--- src/components/TopFiveList.tsx
+++ src/components/TopFiveList.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 
+import ButtonGroup from '@/components/common/ButtonGroup';
 import { useQuery } from '@tanstack/react-query';
 
 interface MockDataType {
@@ -7,30 +10,43 @@ interface MockDataType {
   title: string;
 }
 
-// API가 아직 나오지 않아서 데이터 하드코딩 - 추후 수정 필요
+const CriteriaList = ['거래가 많은(전일)', '보상이 많은(전일)'] as const;
+
 const TopFiveList = () => {
+  const [criteria, setCriteria] = useState('거래가 많은(전일)');
+
   const { data } = useQuery<MockDataType[]>({
     queryKey: ['cahootListData'],
     queryFn: () =>
       fetch(`${process.env.NEXT_PUBLIC_HOST}/cahootListData.json`).then((res) => res.json()),
   });
 
+  const changeCriteria = (criteria: string) => {
+    setCriteria(criteria);
+  };
+
   return (
-    <div className="flex flex-col px-4 md:px-0 py-4 gap-4 w-full md:w-80">
-      <div className="flex justify-between items-center">
+    <div className="flex w-full flex-col gap-2 px-4 py-4 md:w-80 md:px-0">
+      <div className="flex items-center justify-between">
         <label className="font-bold">휴양지 Top 5</label>
       </div>
+      <ButtonGroup
+        items={CriteriaList}
+        currentItem={criteria}
+        changeItem={changeCriteria}
+        buttonSize="large"
+      />
       <div className="flex flex-col gap-1">
         {data?.map(({ id, title }) => (
-          <Link href={`/cahoots/detail/${id}`} key={id} className="flex md:p-1 gap-4">
-            <div className="avatar md:w-1/5 min-w-[76px]">
-              <div className="w-full bg-dark-grey rounded-lg"></div>
+          <Link href={`/cahoots/detail/${id}`} key={id} className="flex gap-4 md:p-1">
+            <div className="avatar min-w-[76px] md:w-1/5">
+              <div className="w-full rounded-lg bg-dark-grey"></div>
             </div>
-            <div className="w-4/5 flex flex-col w-full relative gap-1 justify-center pr-4 text-xs">
+            <div className="relative flex w-4/5 w-full flex-col justify-center gap-1 pr-4 text-xs">
               <div className="w-4/5 font-bold">
                 <p className="truncate">{title}</p>
               </div>
-              <div className="flex gap-4 justify-between">
+              <div className="flex justify-between gap-4">
                 {/* 현재 마켓 거래가 */}
                 <div className="flex flex-1 justify-start text-black/70">
                   <span>{'1,300'}</span>
@@ -40,7 +56,7 @@ const TopFiveList = () => {
                   <span>{'▼ 200(-1%)'}</span>
                 </div>
               </div>
-              <div className="flex gap-4 justify-between">
+              <div className="flex justify-between gap-4">
                 <div className="flex flex-1 justify-start">
                   <span className="text-black/50">1주</span>
                 </div>
