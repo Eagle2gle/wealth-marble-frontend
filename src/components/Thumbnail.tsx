4c7e07e31a380cbbbfad150102030d4e39a79f76
m4nd4r1n <kara.mandarin99@gmail.com>
2023-04-06 23:29:23 +0900

    style: 배너 이미지 최대 너비 제거

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Thumbnail = () => {
   return (
     <div className="hero overflow-hidden pt-28 pb-24">
-      <div className="hero-overlay absolute left-0 right-0 mx-auto h-56 max-w-3xl">
+      <div className="hero-overlay absolute left-0 right-0 h-56">
         <Image
           className="w-full object-cover"
           src="/images/thumbnail.jpeg"
